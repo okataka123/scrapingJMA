36864ed14cdaea9accd3f2565ef86921ec860662
okataka123 <tekepika@gmail.com>
2021-07-19 01:28:42 +0900

    add Location.py and update scrapingJMA.py

---
+++ Location.py
@@ -0,0 +1,96 @@
+#!/usr/bin/env python
+import requests
+from bs4 import BeautifulSoup
+import pandas as pd
+import re
+
+class Location:
+    def __init__(self, prec_str, block_str, fromcsv=True, fromweb=False):
+        """
+        Args:
+            prec_str(str): 気象情報を取得したい場所の都道府県・地方情報
+            block_str(str): 気象情報を取得したい場所の地点情報
+            fromcsv(bool): Trueのとき、csvファイルから位置コード情報を取得する。デフォルトはこちら。
+            fromweb(bool): Trueのとき、気象庁HPから位置コード情報を取得する。
+        """
+        self.prec_str = prec_str
+        self.block_str = block_str
+        self.columns = ['prec', 'prec_no', 'block', 'block_no']
+        self.locations_df = pd.DataFrame([], columns=self.columns)
+        if fromcsv:
+            self.import_prec_block_no_fromCsv()
+        else:
+            self.import_prec_block_no_fromWeb()
+        self.prec_no, self.block_no = self.extract()
+
+    def import_prec_block_no_fromCsv(self):
+        filename = 'locationsTable.csv'
+        self.locations_df = pd.read_csv(filename)
+
+    def import_prec_block_no_fromWeb(self):
+        """
+        気象庁HPから全て地域の位置コード情報を引っ張り、テーブル(self.locations_df)を作る。
+        """
+        # prec
+        precs_table = []
+        url = 'https://www.data.jma.go.jp/obd/stats/etrn/select/prefecture00'
+        html = requests.get(url)
+        soup = BeautifulSoup(html.content, 'html.parser')
+        rows = soup.findAll('area')
+        for row in rows:
+            prec = row['alt']
+            m = re.search(r'.*prec_no=(\d+)', row['href'])
+            prec_no = m.group(1)
+            precs_table.append((prec, prec_no))
+        # block
+        for (prec, prec_no) in precs_table:
+            blocks_table = []
+            rural_url = f'https://www.data.jma.go.jp/obd/stats/etrn/select/prefecture.php?prec_no={prec_no}&block_no=&year=&month=&day=&view='
+            rural_html = requests.get(rural_url)
+            soup = BeautifulSoup(rural_html.content, 'html.parser')
+            rows = soup.findAll('area')
+            for row in rows:
+                block = row['alt']
+                if block[-2:] == '地方' or block[-2:] == '地点' or block[-1:] == '都' \
+                    or block[-1:] == '府' or block[-1:] == '県' or block[-5:] == 'へのリンク':
+                    continue
+                m = re.search(r'.*block_no=(\d+)', row['href'])
+                block_no = m.group(1)
+                blocks_table.append((block, block_no))
+            blocks_table = set(list(blocks_table))
+            blocks_table = sorted(blocks_table, key=lambda x: x[0])
+            for (block, block_no) in blocks_table:
+                data = [prec, prec_no, block, block_no]
+                tmp_df = pd.DataFrame([data], columns=self.columns)
+                self.locations_df = pd.concat([self.locations_df, tmp_df])
+        self.locations_df.reset_index(inplace=True, drop=True)
+
+    def extract(self):
+        """
+        気象情報を取得したい地域について、位置コード番号を引く。
+        """
+        df = self.locations_df
+        cond = df.loc[(df['prec'] == self.prec_str) & (df['block'] == self.block_str)].index
+        prec_no = df.loc[cond, 'prec_no'].values[0]
+        block_no = df.loc[cond, 'block_no'].values[0]
+        return prec_no, block_no
+
+    def csvdump(self):
+        """
+        self.locations_dfに格納された全国の位置コード情報をcsvにdumpする。
+        """
+        filename = 'locationsTable.csv'
+        self.locations_df.to_csv(filename, index=False)
+
+    def verify(self):
+        """
+        T.B.D.
+        HPの情報と、現在持っているテーブルの内容が一致しているか確認する。
+        """
+        pass
+
+def main():
+    pass
+
+if __name__ == '__main__':
+    main()
--- scrapingJMA.py
+++ scrapingJMA.py
@@ -6,40 +6,40 @@ import pandas as pd
 from datetime import timedelta
 import datetime as dt
 import os
+from Location import *
 
 class JMAData:
-    """
-    TODO:
-        場所指定できるようにする。
-        csvに落とせるようにする。
-    """
-    def __init__(self, startyear, startmonth, startday, endyear=None, endmonth=None, endday=None):
+    def __init__(self, prec_str, block_str, startyear, startmonth, startday, endyear=None, endmonth=None, endday=None):
+        """
+        Args:
+            prec_str(str): 都道府県・地方情報
+            block_str(str): 地点情報
+            startyear(int): 開始年
+            startmonth(int): 開始月
+            startday(int): 開始日
+            endyear(int): 終了年
+            endmonth(int): 終了月
+            endday(int): 終了日
+        """
+        self.prec_str = prec_str
+        self.block_str = block_str
         if endyear is None:
             endyear = startyear
         if endmonth is None:
             endmonth = startmonth
         if endday is None:
             endday = startday
-
-        # T.B.D.
-        self.prec_str = ""
-        # T.B.D.
-        self.block_str = ""
-
+        self.prec_no, self.block_no = self.getlocationcode()
         self.startDateTime = pd.to_datetime(str(startyear)+'/'+str(startmonth)+'/'+str(startday))
         self.endDateTime = pd.to_datetime(str(endyear)+'/'+str(endmonth)+'/'+str(endday))
-        self.header = ['時刻', '現地気圧(hPa)', '海面気圧(hPa)', '降水量(mm)', '気温(℃)', '相対湿度(％)', '平均風速(m/s)', '平均風向', '最大瞬間風速(m/s)', '最大瞬間風向']
+        self.header = ['時刻', '現地気圧(hPa)', '海面気圧(hPa)', '降水量(mm)', '気温(℃)',  '相対湿度(％)',
+            '平均風速(m/s)', '平均風向', '最大瞬間風速(m/s)', '最大瞬間風向',
+        ]
         self.jma_df = pd.DataFrame(columns=self.header)
 
-    def getlocationcode(self, prec_str, block_str):
-        """
-        Returns:
-            proc_no(int): T.B.D.
-            block_no(int): T.B.D.
-        Note:
-            別途Locationクラスを作る？
-        """
-        pass
+    def getlocationcode(self):
+        loc = Location(self.prec_str, self.block_str)
+        return loc.prec_no, loc.block_no
 
     def str2float(self, string):
         try:
@@ -54,9 +54,8 @@ class JMAData:
         today = pd.to_datetime(str(year)+'/'+str(month)+'/'+str(day))
         yesterday = today - timedelta(days=1)
 
-        # 現状は静岡県静岡市限定
         # 昨日の24:00 
-        url = f'http://www.data.jma.go.jp/obd/stats/etrn/view/10min_s1.php?prec_no=50&block_no=47656&year={yesterday.year}&month={yesterday.month}&day={yesterday.day}&view='
+        url = f'http://www.data.jma.go.jp/obd/stats/etrn/view/10min_s1.php?prec_no={self.prec_no}&block_no={self.block_no}&year={yesterday.year}&month={yesterday.month}&day={yesterday.day}&view='
         html = requests.get(url)
         soup = BeautifulSoup(html.content, 'html.parser')
         rows = soup.findAll('tr', class_='mtx')
@@ -81,7 +80,7 @@ class JMAData:
                 self.jma_df = pd.concat([self.jma_df, rowData])
 
         # 今日の00:00-23:50
-        url = f'http://www.data.jma.go.jp/obd/stats/etrn/view/10min_s1.php?prec_no=50&block_no=47656&year={year}&month={month}&day={day}&view='
+        url = f'http://www.data.jma.go.jp/obd/stats/etrn/view/10min_s1.php?prec_no={self.prec_no}&block_no={self.block_no}&year={year}&month={month}&day={day}&view='
         html = requests.get(url)
         soup = BeautifulSoup(html.content, 'html.parser')
         rows = soup.findAll('tr', class_='mtx')
@@ -120,7 +119,11 @@ class JMAData:
                 
     def dumpcsv(self, everydays=False):
         """
-        # jmaData_shizuoka_shizuoka_20210701_20210701.csv
+        気象データDataFrameをcsvにdumpする。
+
+        Args:
+            everydays(bool): これがTrueのとき、開始日〜終了日までの1日ごとのcsvにdumpされる。
+                             これがFalseならば、DataFrameは一つのファイルに全てdumpされる。
         """
         dumpdirname = 'csvout'
         if not os.path.exists(dumpdirname):
@@ -128,8 +131,7 @@ class JMAData:
         if not everydays:
             startday_str = self.startDateTime.strftime('%Y%m%d')
             endday_str = self.endDateTime.strftime('%Y%m%d')
-            #self.jma_df.to_csv(f'./csvout/jmaData_{}_{}_{startday_str}_{endday_str}.csv', index=False)
-            self.jma_df.to_csv(f'./csvout/jmaData_a_a_{startday_str}_{endday_str}.csv', index=False)
+            self.jma_df.to_csv(f'./csvout/jmaData_{self.prec_str}_{self.block_str}_{startday_str}_{endday_str}.csv', index=False)
         else:
             datetime = self.startDateTime
             while True:
@@ -137,15 +139,11 @@ class JMAData:
                     break
                 day_str = datetime.strftime('%Y%m%d')
                 day_df = self.jma_df.loc[self.jma_df['時刻'].dt.date==datetime]
-                #day_df.to_csv(f'./csvout/jmaData_{}_{}_{day_str}_{day_str}.csv', index=False)
-                day_df.to_csv(f'./csvout/jmaData_a_a_{day_str}_{day_str}.csv', index=False)
+                day_df.to_csv(f'./csvout/jmaData_{self.prec_str}_{self.block_str}_{day_str}_{day_str}.csv', index=False)
                 datetime += timedelta(days=1)
 
 def main():
-    jma = JMAData(2021, 7, 1, 2021, 7, 5)
-    jma_df = jma.constructWeatherData()
-    #jma.dumpcsv()
-    jma.dumpcsv(everydays=True)
+    pass
 
 if __name__ == '__main__':
     main()
